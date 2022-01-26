0bab8dc78762e2dfb3bd8a178a8c194dc93b938f
Theo <theomaes54@gmail.com>
2022-01-26 17:31:03 +0100

    update initial commit

---
+++ src/main/java/fr/perso/movieQuizz/MovieQuizzApplication.java
@@ -0,0 +1,13 @@
+package fr.perso.movieQuizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieQuizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieQuizzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/perso/movieQuizz/MovieQuizzApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.perso.movieQuizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieQuizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
